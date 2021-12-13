adfb115e0da1ad4f657662c27d8fc25aad9d908e
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2021-12-13 03:57:37 -0600

    Se agregan comentarios al programa

--- Empleado.java
+++ Empleado.java
@@ -1,28 +1,74 @@
-public class Empleado{
+/**
+*Clase que simula un empleado.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*/
 
+public class Empleado{
+    //Atributo que representa el nombre del empleado
     protected String nombre;
 
+    /**
+*Metodo constructor sin parametros que permite crear un objeto de la clase empleado con valores predeterminados en sus atributos.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*/
     public Empleado(){
 	
 	nombre="Alan Brito";
     }
-
+ /**
+*Metodo constructor con parametros que permite crear un objeto de la clase empleado dándole valor a sus atributos.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*@param nombre parámetro que representa el nombre del empleado
+*/
     public Empleado(String nombre){
 	this.nombre=nombre;
 
     }
 
+     /**
+*Metodo que sirve para obtener el valor del atributo "nombre" de un objeto de la clase empleado.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*@return String
+*/
     public String getNombre(){
 	return this.nombre;
     }
+
+         /**
+*Metodo que sirve para modificar el valor del atributo "nombre" de un objeto de la clase empleado.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*@param nombre parámetro que representa el nombre del empleado
+*/
 
     public void setNombre(String nombre){
 	this.nombre=nombre;
     }
 
+    
+     /**
+*Metodo que muestra el objeto en cadena.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*@return String
+*/
+
+   @Override
     public String toString(){
 	return "Empleado: "+this.nombre;
     }
+
+         /**
+*Metodo que sirve para comparar dos objetos de la clase y determinar si son iguales.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*@param x parámetro que representa un objeto de la clase empleado.
+*@return boolean 
+*/
 
     public boolean equals(Object x){
 	Empleado y = (Empleado) x;
--- Ordenamientos.java
+++ Ordenamientos.java
@@ -1,7 +1,11 @@
 import  java.util.Random;
     import java.util.Scanner;
 
-
+/**
+ *Programa que sirve para ordenar un arreglo de números de menor a mayor usando distintos métodos.
+ *@author Alcantara Estrada Kevin Isaac
+ *@version 1.0
+ */
 public class Ordenamientos{
     
     private static int particion(int arreglo[], int izquierda, int derecha) {
@@ -36,12 +40,23 @@ public class Ordenamientos{
     }
 }
 
+
+         /**
+*Metodo que sirve pbara ordenar los elementos de un arreglo de menor a mayor usando el algoritmo buble sort.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*@param arreglo[] Parámetro que representa el arreglo de números enteros a ordenar
+*/
+
     public static void bubblesort(int arreglo[]){
+	//Se declaran variables que servirán para el método
 	int i,num_aux;
 	boolean cambiado;
-	
+	//Abrimos un ciclo while que, por la condición, se ejecutará de forma infinita
 	while(true){
+	    //Asignamos el valor de falso a la variable cambios
 	    cambiado=false;
+	    
 	    for(i=1;i<arreglo.length;i++){
 		if(arreglo[i]<arreglo[i-1]){
 		    num_aux=arreglo[i];
@@ -56,35 +71,49 @@ public class Ordenamientos{
 	    }
 	}
     }
-    
+
+
+         /**
+*Metodo main que sirve para probar los dos algoritmos de ordenamiento.
+*@author Alcantara Estrada Kevin Isaac
+*@version 1.0
+*@param args
+*/
+
 public static void main(String[] args){
 
-     
+    //Se declaran variables auxiliares para el buen funcionamiento del programa.
     int resp,rango,i,x,b,y;
    
-
+    //Creación de un objeto de la clase Scanner para recibir datos por consola
     Scanner sc = new Scanner(System.in);
+
+    //Se solicita el numero de elementos aleatorios que quiere ordenar el usuario
 	System.out.println("Escribe el número de elementos aleatorios que deseas ordenar");
 	x = sc.nextInt();
 	b=x;
+	//Se crea un arreglo de enteros para guardar dichos numeros aleatorios
 	 int A[] = new int[x];
 	y=x-1;
 	
+	//Se solicita al usuario que indique desde 0 hasta que rango quiere que sea el valor de los elementos a ordenar
 			System.out.println("Escribe el máximo para el rango de números enteros en el cuál quieres que se generen números aleatorios");
 	rango = sc.nextInt();
-
+	//Menú para que el usuario seleccione el agoritmo a utilizar
 		System.out.println("Elige qué método de ordenamineto desea utilizar \n1.Quick Sort \n2.Bubble Sort");
 		resp=sc.nextInt();
+		//Ciclo para generar los números aleatorios
 	for(i=0; i<=y; i++){
 		    A[i]= (int)(Math.random()*rango);
-		    //	System.out.println(A[i]+ " ");
+		    
 		    
       	}
+	//Estrcutura de control para usar los distintos algoritmos de ordenamientos según sea el caso
 		switch(resp){
 
 		case 1:
 		    quicksort(A,0,y);
-
+		    //Bucle para imprimir los números en pantalla
 		for(i=0;i<=y;i++){
 		    System.out.print(A[i]+" ");
 		}
@@ -92,7 +121,7 @@ public static void main(String[] args){
 
 	       	case 2:
 		    bubblesort(A);
-
+		    //Bucle para imprimir los números en pantalla
 		for(i=0;i<=y;i++){
 		    System.out.print(A[i]+" ");
 		}
