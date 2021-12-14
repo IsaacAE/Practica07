a1207914e4211a8be4779125140fda07f9e0e525
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2021-12-13 23:20:17 -0600

    Se termina la documentación

--- Ordenamientos.java
+++ Ordenamientos.java
@@ -4,50 +4,63 @@ import  java.util.Random;
 /**
  *Programa que sirve para ordenar un arreglo de números de menor a mayor usando distintos métodos.
  *@author Alcantara Estrada Kevin Isaac
- *@version 1.0
+ *@version 3.0
  */
 public class Ordenamientos{
-    
-    private static int particion(int arreglo[], int izquierda, int derecha) {
-        int pivote = arreglo[izquierda];
+    //Variable global auxiliar para intercambiar la posición entre elementos de un arreglo
+    static int num_aux;
 
-// Ciclo infinito
-        while (true) {
-	    //Este ciclo se frena cuando el valor de un elemento del arreglo no es menor que el valor de nuestro pivote
-	     while (arreglo[izquierda] < pivote) {
-	     izquierda++;
-		}
-	     //Este ciclo se frena cuando encuentra un elemento del arreglo cuyo valor no es mayor que el valor de nuestro pivote 
-            while (arreglo[derecha] > pivote) {
-                derecha--;
-            } 
-            if (izquierda >= derecha) {
-		 int temporal = arreglo[izquierda];
-                arreglo[izquierda] = arreglo[derecha];
-                arreglo[derecha] = temporal;
-                izquierda++;
-                derecha--;
-		 return derecha;
-		
-            }/* else {
-                int temporal = arreglo[izquierda];
-                arreglo[izquierda] = arreglo[derecha];
-                arreglo[derecha] = temporal;
-                izquierda++;
-                derecha--;
-		
-		}*/
-            // El while se repite hasta que izquierda >= derecha
+    /**
+ *Método que ordena un arreglo de números enteros aplicando el algoritmo 
+ *@author Alcantara Estrada Kevin Isaac
+ *@version 1.0
+ *@param array Es el arreglo a ordenar
+ *@param inicio Indice del primer elemento del arreglo
+ *@param ultimo Indice del ultimo elemento del arreglo
+ */    
+public static void quicksort(int[] array,int inicio,int ultimo){
+    //Declaramos variables auxiliares
+    int indice_izq,indice_der,pivote;
+    //Caso base de nuestra recursión
+     if(inicio>ultimo){
+            return;
+	    }
+     //Asignación de valores a nuestras variables auxiliares
+        indice_izq=inicio;
+        indice_der=ultimo;
+	//Primero se hará el ordenamiento tomando como base el valor de la variable pivote, que será el valor del primer elemento del arreglo
+        pivote = array[inicio];
+	//El ciclo se realizará hasta que nuestras variables auxiliares sen encuentre
+        while (indice_der>indice_izq) {
+                         // Buscamos un valor que sea menor que nuestro pivote desde el último elemento de nuestro arreglo y hacia la izquierda
+            while (indice_izq<indice_der&&pivote<=array[indice_der]) {
+                indice_der--;
+            }
+                         //Partiendo desde el primer elemento de nuestro arreglo, buscamos un elemento que sea mayor que el valor de nuestro pivote y seguimos la búsqueda hacia la derecha
+            while (pivote>=array[indice_izq]&&indice_izq<indice_der) {
+                indice_izq++;
+            }
+            // Una vez que hemos encontrado los elementos que buscábamos, si el elemento que buscamos desde la izquierda esta a la derecha del que buscamos por la derecha, intercambiamos los valores de dichos elementos en el arreglo
+            if (indice_izq<indice_der) {
+                num_aux = array[indice_der];
+                array[indice_der] = array[indice_izq];
+                array[indice_izq] = num_aux;
+            }
+ 
         }
-    }
+                 // Finalmente, al encontrarse nuestros indices auxiliares, intercambiamos el valor nuestro pivote (el valor del primer elemento del arreglo) por el valor del elemento del arreglo en el cual se encontraron nuestros indices auxiliares
+         array[inicio] = array[indice_izq];
+         array[indice_izq] = pivote;
+	 //NOTA: de esta manera ya tenemos el pivote ubicado en el lugar que le corresponde en el arreglo y, a su derecha, los elementos mayores a este y, a su izquierda, los valores menores a esta
 
-    private static void quicksort(int arreglo[], int izquierda, int derecha) {
-    if (izquierda < derecha) {
-        int indiceParticion = particion(arreglo, izquierda, derecha);
-        quicksort(arreglo, izquierda, indiceParticion);
-        quicksort(arreglo, indiceParticion + 1, derecha);
+
+	 //COMENZAMOS LAS LLAMADAS RECURSIVAS
+
+	   //Ordenamos los números que están a la izquierda de nuestro pivote 
+        quicksort(array, inicio, indice_der-1);
+                 //Ordenamos los números que están a la derecha de nuestro pivote
+        quicksort(array, indice_der+1, ultimo);
     }
-}
 
 
          /**
@@ -59,7 +72,7 @@ public class Ordenamientos{
 
     public static void bubblesort(int arreglo[]){
 	//Se declaran variables que servirán para el método
-	int i,num_aux;
+	int i;
 	boolean cambiado;
 	//Abrimos un ciclo while que, por la condición, se ejecutará de forma infinita, para que el ordenamiento se realice para cada uno de los elementos del arreglo
 		while(true){
@@ -110,16 +123,20 @@ public static void main(String[] args){
 	
 	//Se solicita al usuario que indique desde 0 hasta que rango quiere que sea el valor de los elementos a ordenar
 			System.out.println("Escribe el máximo para el rango de números enteros en el cuál quieres que se generen números aleatorios");
+			
 	rango = sc.nextInt();
-	//Menú para que el usuario seleccione el agoritmo a utilizar
-		System.out.println("Elige qué método de ordenamineto desea utilizar \n1.Quick Sort \n2.Bubble Sort");
-		resp=sc.nextInt();
-		//Ciclo para generar los números aleatorios
-	for(i=0; i<=y; i++){
+		for(i=0; i<=y; i++){
 		    A[i]= (int)(Math.random()*rango);
 		    
 		    
       	}
+	       
+
+	//Menú para que el usuario seleccione el agoritmo a utilizar
+		System.out.println("Elige qué método de ordenamineto desea utilizar \n1.Quick Sort \n2.Bubble Sort");
+		resp=sc.nextInt();
+		//Ciclo para generar los números aleatorios
+
 	//Estrcutura de control para usar los distintos algoritmos de ordenamientos según sea el caso
 		switch(resp){
 
